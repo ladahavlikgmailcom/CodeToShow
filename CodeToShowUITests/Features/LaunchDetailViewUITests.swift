81875f53995e44530a21abf8566d757e4e8f8a2b
[ laďa ] HAVLÍK <lada.havlik@gmail.com>
2023-06-12 12:55:15 +0200

    Fixed UI Test in Xcode 15.

--- CodeToShowUITests/Features/LaunchDetailViewUITests.swift
+++ CodeToShowUITests/Features/LaunchDetailViewUITests.swift
@@ -22,7 +22,8 @@ final class LaunchDetailViewUITests: XCTestCase {
         searchSearchField.tap()
         searchSearchField.typeText("Crew-5")
         // swiftlint:disable:next line_length
-        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success"]/*[[".cells.buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success\"]",".buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let crew5Button = app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success"]/*[[".cells.buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]",".buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        crew5Button.tap()
 
         let elementsQuery = XCUIApplication().scrollViews.otherElements
         elementsQuery.staticTexts["Crew-5"].tap()
@@ -37,7 +38,9 @@ final class LaunchDetailViewUITests: XCTestCase {
         searchSearchField.buttons["Clear text"].tap()
         searchSearchField.typeText("ABS-2A")
         // swiftlint:disable:next line_length
-        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["ABS-2A / Eutelsat 117W B, Flight number 31, Date 15. 6. 2016 14:29, Mission Success"]/*[[".cells.buttons[\"ABS-2A \/ Eutelsat 117W B, Flight number 31, Date 15. 6. 2016 14:29, Mission Success\"]",".buttons[\"ABS-2A \/ Eutelsat 117W B, Flight number 31, Date 15. 6. 2016 14:29, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let abs2aButton = app.collectionViews/*@START_MENU_TOKEN@*/.buttons["ABS-2A / Eutelsat 117W B, Flight number 31, Date 15. 6. 2016, 14:29, Mission Success"]/*[[".cells.buttons[\"ABS-2A \/ Eutelsat 117W B, Flight number 31, Date 15. 6. 2016, 14:29, Mission Success\"]",".buttons[\"ABS-2A \/ Eutelsat 117W B, Flight number 31, Date 15. 6. 2016, 14:29, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        abs2aButton.tap()
+
         elementsQuery.staticTexts["ABS-2A / Eutelsat 117W B"].tap()
         elementsQuery.staticTexts["Rocket"].tap()
         elementsQuery.staticTexts["Name, Falcon 9"].tap()
--- CodeToShowUITests/Features/LaunchesListViewUITests.swift
+++ CodeToShowUITests/Features/LaunchesListViewUITests.swift
@@ -18,10 +18,10 @@ final class LaunchesListViewUITests: XCTestCase {
     }
 
     func testLaunchesListViewToAppearOnScreen() throws {
-        let missionsNavigationBar = XCUIApplication().navigationBars["Missions"]
-        missionsNavigationBar.staticTexts["Missions"].tap()
-        // swiftlint:disable:next line_length
-        missionsNavigationBar/*@START_MENU_TOKEN@*/.images["Sort"]/*[[".otherElements[\"Sort\"]",".buttons[\"Sort\"]",".buttons.images[\"Sort\"]",".images[\"Sort\"]"],[[[-1,3],[-1,2],[-1,1,2],[-1,0,1]],[[-1,3],[-1,2],[-1,1,2]],[[-1,3],[-1,2]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let missionsNavigationBar = app.navigationBars["Missions"]
+        missionsNavigationBar.searchFields["Search"].tap()
+        missionsNavigationBar.buttons["Cancel"].tap()
+        missionsNavigationBar.buttons["Sort"].tap()
     }
 
     func testLaunchesListViewSearchCrew5() throws {
@@ -31,7 +31,7 @@ final class LaunchesListViewUITests: XCTestCase {
     func testLaunchesListViewNavigateToDetail() throws {
         searchCrew5()
         // swiftlint:disable:next line_length
-        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success"]/*[[".cells.buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success\"]",".buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022 16:00, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success"]/*[[".cells.buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]",".buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.scrollViews.otherElements.staticTexts["Crew-5"].tap()
     }
 
