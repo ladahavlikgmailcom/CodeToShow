5ec231034461075ab3a9fa9d48d23301b1bb218a
[ laďa ] HAVLÍK <lada.havlik@gmail.com>
2023-06-23 13:00:53 +0200

    Adjusted test after set a localization.

--- CodeToShowUITests/Features/LaunchesListViewUITests.swift
+++ CodeToShowUITests/Features/LaunchesListViewUITests.swift
@@ -18,10 +18,10 @@ final class LaunchesListViewUITests: XCTestCase {
     }
 
     func testLaunchesListViewToAppearOnScreen() throws {
-        let missionsNavigationBar = app.navigationBars["Missions"]
-        missionsNavigationBar.searchFields["Search"].tap()
-        missionsNavigationBar.buttons["Cancel"].tap()
-        missionsNavigationBar.buttons["Sort"].tap()
+        let missionsNavigationBar = app.navigationBars["Mise"]
+        missionsNavigationBar.searchFields["Hledat"].tap()
+        missionsNavigationBar.buttons["Zrušit"].tap()
+        missionsNavigationBar.buttons["Seřadit"].tap()
     }
 
     func testLaunchesListViewSearchCrew5() throws {
@@ -30,13 +30,12 @@ final class LaunchesListViewUITests: XCTestCase {
 
     func testLaunchesListViewNavigateToDetail() throws {
         searchCrew5()
-        // swiftlint:disable:next line_length
-        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success"]/*[[".cells.buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]",".buttons[\"Crew-5, Flight number 187, Date 5. 10. 2022, 16:00, Mission Success\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews.staticTexts["Crew-5"].tap()
         app.scrollViews.otherElements.staticTexts["Crew-5"].tap()
     }
 
     func searchCrew5() {
-        let searchSearchField = app.navigationBars["Missions"].searchFields["Search"]
+        let searchSearchField = app.navigationBars["Mise"].searchFields["Hledat"]
         searchSearchField.tap()
         searchSearchField.typeText("Crew-5")
     }
