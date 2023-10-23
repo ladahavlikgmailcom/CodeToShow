2e2895f201e640294ca673d2aaa9d8a5de1d277d
[ laďa ] HAVLÍK <lada.havlik@gmail.com>
2023-10-23 12:12:15 +0200

    Target for iPad.

---
+++ CodeToShowPad/CodeToShowPadApp.swift
@@ -0,0 +1,18 @@
+//
+//  CodeToShowPadApp.swift
+//  CodeToShowPad
+//
+//  Created by [ laďa ] HAVLÍK on 23.10.2023.
+//  Copyright © 2023 LHBnO. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CodeToShowPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
